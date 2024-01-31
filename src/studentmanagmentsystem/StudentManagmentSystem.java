70f9bc160cb2f78dc210204aa9d9ae37902632dc
Shawnarun <shawnarun19@gmail.com>
2024-01-31 21:01:28 +0530

    FEAT: Add New Method

--- src/studentmanagmentsystem/StudentManagmentSystem.java
+++ src/studentmanagmentsystem/StudentManagmentSystem.java
@@ -36,12 +36,27 @@ public class StudentManagmentSystem {
         String user="root";
         String password="123456";
         
-        String sql="INSERT INTO student (name,age,department,district,nic,gender,performance) VALUES ('Sarangan shagar',52,'Engineering','Batticaloa','97655585V','Male',90)";
+        String name= "Hasika";
+        int age =21;
+        String department="Engineering";
+        String district="Batticaloa";
+        String nic="2000655585V";
+        String gender="Male";
+        int performance=95;
+        
+        String sql="INSERT INTO student (name,age,department,district,nic,gender,performance)"
+                + " VALUES (?,?,?,?,?,?,?)";
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             
-            Statement st=con.createStatement();
-            int row=st.executeUpdate(sql);
-           
+            PreparedStatement ps=con.prepareStatement(sql);
+                ps.setString(1, name);
+                ps.setInt(2, age);
+                ps.setString(3, department);
+                ps.setString(4, district);
+                ps.setString(5, nic);
+                ps.setString(6, gender);
+                ps.setInt(7, performance);
+           int row= ps.executeUpdate();
             System.out.println(row);
         }
         catch(Exception e){
